aaddbdc7fff1cdab221e6bffc7d2d7cad3bf76a1
Khasbullah Zakin <hasbizakin0804@gmail.com>
2022-10-25 14:56:42 +0700

    ADD

---
+++ src/Variable.java
@@ -0,0 +1,21 @@
+public class Variable {
+    public static void main(String[] args) throws Exception {
+                // integer
+                byte aSingleByte = 100; // -128 to 127
+                short aSmallNumber = 20000; // -32768 to 32767
+                int anInteger = 2147483647; // -2147483648 to 2147483647
+                long alargeNumber = 9223372036854775807L; // -9223372036854775808 to 9223372036854775807
+                
+                // decimal
+                double aDouble = 1.79769313; // 4.9E-324 to 1.7976931348623157E308
+                float aFloat = 3.4028F; // 1.4E-45 to 3.4028235E38
+        
+                // boolean
+                boolean isWeekend = false;
+                boolean isWorkday = true;
+        
+                // characters
+                char copyrightSymbol = '\u00A9';
+                System.out.println(aSingleByte + " " + aSmallNumber + " " + anInteger + " " + alargeNumber + " " + aDouble + " " + aFloat + " " + isWeekend + " " + isWorkday + " the copyright symbol is " + copyrightSymbol);
+    }
+}
