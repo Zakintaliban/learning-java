d81f965522614c390731b3807e4c3b3c65b245e7
Khasbullah Zakin <hasbizakin0804@gmail.com>
2022-10-25 14:56:38 +0700

    ADD

---
+++ src/App.java
@@ -0,0 +1,13 @@
+class App {
+    public static void main(String[] args) throws Exception {
+        // print
+        String x = "'Hello, World!'";
+        System.out.println("the word" + x + "is the first word that used by Brian Kernighan, inventor of C");
+    }
+}
+    // kalo bikin class baru, maka didalam folder bin terdapat file baru sesuai nama class (Rest.class)
+    // class Rest {
+    //     public static void main(String[] args) {
+    //         System.out.println("Hello, Moon!");
+    //     }
+    // }
